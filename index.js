f6f3edc56e02f8e79e6394ffc1ea4f84fbd186bf
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-10 20:32:57 -0300

    creating server

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Primary file for the API
+ */
+
+const http = require('http')
+const url = require('url')
+
+const server = http.createServer((req, res)=>{
+    const parsedUrl = url.parse(req.url, true)
+    const path = parsedUrl.pathname
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+    res.end('Hello World!\n')
+    console.log('Request received on path:', trimmedPath)
+})
+
+server.listen(3000, ()=>{ console.log('Server listening on port 3000')})

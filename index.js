c2a3153c8bf1a1d67a54cac4815b77f6498edb31
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-11 15:35:20 -0300

    post users route

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const http = require('http')
 const https = require('https')
 const url = require('url')
 const {StringDecoder} = require('string_decoder')
-const config = require('./config')
+const config = require('./lib/config')
 const fs  = require('fs')
 const handlers = require('./lib/handlers')
+const helpers = require('./lib/helpers')
 
 const unifiedServer = (req, res) =>{
     const parsedUrl = url.parse(req.url, true)
@@ -40,17 +41,16 @@ const unifiedServer = (req, res) =>{
             queryStringObject,
             method,
             headers,
-            payload: buffer
+            payload: helpers.parseJsonToObject(buffer)
         }
 
         chosenHandler(data, (statusCode = 200, payload = {})=>{
-
             const payloadString = JSON.stringify(payload)
+            console.log('Returning response:', statusCode, payloadString )
 
             res.setHeader('Content-Type', 'application/json')
             res.writeHead(statusCode)
             res.end(payloadString)
-            console.log('Returning response:', statusCode,payloadString )
         })
 
     })
@@ -71,7 +71,8 @@ httpsServer.listen(config.httpsPort, ()=>{ console.log(`Server listening on port
 // request router
 
 const router = {
-    'ping': handlers.ping
+    'ping': handlers.ping,
+    'users': handlers.users
 }
 
 

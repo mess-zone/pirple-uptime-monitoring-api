2073bd060161266543aa59a5b636739634212378
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-15 17:33:22 -0300

    post checks

--- index.js
+++ index.js
@@ -73,7 +73,8 @@ httpsServer.listen(config.httpsPort, ()=>{ console.log(`Server listening on port
 const router = {
     'ping': handlers.ping,
     'users': handlers.users,
-    'tokens': handlers.tokens
+    'tokens': handlers.tokens,
+    'checks': handlers.checks
 }
 
 

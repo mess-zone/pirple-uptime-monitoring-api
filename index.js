87aa898117db33993b34d16c12b2eb494a57ac9a
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-17 16:45:45 -0300

    refatoração server

--- index.js
+++ index.js
@@ -2,80 +2,19 @@
  * Primary file for the API
  */
 
-const http = require('http')
-const https = require('https')
-const url = require('url')
-const {StringDecoder} = require('string_decoder')
-const config = require('./lib/config')
-const fs  = require('fs')
-const handlers = require('./lib/handlers')
-const helpers = require('./lib/helpers')
+const server = require('./lib/server')
+const workers = require('./lib/workers')
 
-const unifiedServer = (req, res) =>{
-    const parsedUrl = url.parse(req.url, true)
+const app = {}
 
-    const path = parsedUrl.pathname
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+app.init = () => {
+    //start server
+    server.init()
 
-    const queryStringObject = parsedUrl.query
-
-    const method = req.method.toLowerCase()
-
-    const headers = req.headers
-
-
-    // get body payload, if any
-    const decoder = new StringDecoder('utf-8') 
-    let buffer = ''
-
-    req.on('data', (data)=>{
-        buffer += decoder.write(data)
-    })
-
-    req.on('end', ()=>{
-        buffer += decoder.end()
-
-        const chosenHandler = router[trimmedPath] || handlers.notFound
-        const data = {
-            trimmedPath,
-            queryStringObject,
-            method,
-            headers,
-            payload: helpers.parseJsonToObject(buffer)
-        }
-
-        chosenHandler(data, (statusCode = 200, payload = {})=>{
-            const payloadString = JSON.stringify(payload)
-            console.log('Returning response:', statusCode, payloadString )
-
-            res.setHeader('Content-Type', 'application/json')
-            res.writeHead(statusCode)
-            res.end(payloadString)
-        })
-
-    })
+    //start workers
+    // workers.init()
 }
 
-// HTTP server
-const httpServer = http.createServer(unifiedServer)
-httpServer.listen(config.httpPort, ()=>{ console.log(`Server listening on port ${config.httpPort}`)})
+app.init()
 
-// HTTPS server
-const httpsServerOption = {
-    key: fs.readFileSync('./https/key.pem'),
-    cert: fs.readFileSync('./https/cert.pem')
-}
-const httpsServer = https.createServer(httpsServerOption, unifiedServer)
-httpsServer.listen(config.httpsPort, ()=>{ console.log(`Server listening on port ${config.httpsPort}`)})
-
-// request router
-
-const router = {
-    'ping': handlers.ping,
-    'users': handlers.users,
-    'tokens': handlers.tokens,
-    'checks': handlers.checks
-}
-
-
-
+module.exports = app

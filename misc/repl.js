7d93984afa765096099c24869d1660f423ea4f79
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-20 15:23:27 -0300

    REPL

---
+++ misc/repl.js
@@ -0,0 +1,16 @@
+/**
+ * Example REPL server
+ */
+
+const repl = require('repl')
+
+repl.start({
+    'prompt': '>',
+    'eval': (str) => {
+        console.log('Evaluation stage:', str)
+
+        if(str.indexOf('fizz') > -1) {
+            console.log('buzz')
+        }
+    }
+})
